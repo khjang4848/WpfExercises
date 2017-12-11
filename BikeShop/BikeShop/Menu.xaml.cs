afe8f0af027739739b6803ea2cd7224e9a4de2b5
KangHyun Jang <khjang4848@gmail.com>
2017-12-11 19:00:53 +0900

    Init Setting

---
+++ BikeShop/BikeShop/Menu.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace BikeShop
+{
+    /// <summary>
+    /// Interaction logic for Menu.xaml
+    /// </summary>
+    public partial class Menu : Page
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Contact.xaml", UriKind.Relative));
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Discussion.xaml", UriKind.Relative));
+        }
+    }
+}
---
+++ BikeShop/BikeShop/Notifier.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace BikeShop
+{
+    public class Notifier : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this,
+                new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
